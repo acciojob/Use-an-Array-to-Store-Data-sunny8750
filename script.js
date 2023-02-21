18bdc15af377ef898d69f3039ef5249f10b71960
sunny8750 <120244832+sunny8750@users.noreply.github.com>
2023-02-21 16:04:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-
-let arr =  []
-let simpleArray = ['one', 2, 'three', true, false, undefined, null]; 
-console.log(simpleArray);
+let simpleArray = ['one', 2, 'three', 'true', 'false', 'undefined','null']
+let obj=[{
+	"one":simpleArray[1],
+	"two":simpleArray[2],
+	"three":simpleArray[3],
+	"four":simpleArray[4],
+	"five":simpleArray[5],
+	"six":simpleArray[6],
+}]
+console.log(obj);
