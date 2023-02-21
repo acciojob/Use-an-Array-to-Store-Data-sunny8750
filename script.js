648f01663b268e0e9366f69ba8a160303fcd9c2e
sunny8750 <120244832+sunny8750@users.noreply.github.com>
2023-02-21 16:23:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-let simpleArray = ['one', 2, 'three', 'true', 'false', 'undefined','null']
-let obj=[{
-	"one":simpleArray[1],
-	"two":simpleArray[2],
-	"three":simpleArray[3],
-	"four":simpleArray[4],
-	"five":simpleArray[5],
-	"six":simpleArray[6],
-}]
-console.log(obj);
+//let simpleArray = ['one', 2, 'three', 'true', 'false', 'undefined','null']
+let arr = ["John Doe", ["Lily Rose", ["true", ["baseSalary", 23000]]]];
+
+// converted nested array into single array which is 3 levels deep
+let convertedArr = arr.flat(3);
+
+console.log(convertedArr); 
